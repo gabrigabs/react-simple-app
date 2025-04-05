f3c699645b070d54bc3efdb6546bb50ef39e92e0
Gabriel <gabrielbrodrigues1@gmail.com>
2025-04-05 12:15:48 -0300

    implement Contato component layout

--- src/components/contato/Contato.jsx
+++ src/components/contato/Contato.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Contato.css'
+import foto from '../../img/contato.jpg'
 
 export const Contato = () => {
   return (
-    <div>
-        <h1>Contato</h1>
-    </div>
+    <section className='contato anime-left'>
+        <img src={foto} alt="maquina de escrever" />
+        <div>
+          <h1>Entre em contato</h1>
+          <ul className='dados-contato'>
+            <li>Nome: Gabriel Bezerra</li>
+            <li>Email: contato@email.com</li>
+            <li>Telefone: (11) 1234-5678</li>
+          </ul>
+        </div>
+    </section>
   )
 }
